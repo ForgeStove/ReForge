503dadd260899cd1b7c5bdbae4612ea64d17e583
ForgeStove <ForgeStove@outlook.com>
2025-08-14 01:09:09 +0800

    添加配置文件以支持设置本地化和加载选项

--- src/reforge/HudModifier.java
+++ src/reforge/HudModifier.java
@@ -8,36 +8,36 @@ public class HudModifier {
 	public static void load() {
 		Events.on(ClientLoadEvent.class, event -> {
 				var graphics = Vars.ui.settings.graphics;
-				graphics.checkPref("AllowEditRules", true);
-				graphics.checkPref("AllowEditWorldProcessors", true);
-				graphics.checkPref("Bounded", false);
-				graphics.checkPref("Fog", true);
-				graphics.checkPref("StaticFog", true);
-				graphics.checkPref("Darkness", true);
-				graphics.checkPref("ControlPathFinder", false);
-				graphics.checkPref("ShowSpawns", true);
-				graphics.checkPref("PossessionAllowed", true);
-				graphics.checkPref("SchematicsAllowed", true);
-				graphics.checkPref("LimitMapArea", false);
+				graphics.checkPref(Core.bundle.get("settings.allowEditRules"), true);
+				graphics.checkPref(Core.bundle.get("settings.allowEditWorldProcessors"), true);
+				graphics.checkPref(Core.bundle.get("settings.bounded"), false);
+				graphics.checkPref(Core.bundle.get("settings.fog"), true);
+				graphics.checkPref(Core.bundle.get("settings.staticFog"), true);
+				graphics.checkPref(Core.bundle.get("settings.darkness"), true);
+				graphics.checkPref(Core.bundle.get("settings.controlPathFinder"), false);
+				graphics.checkPref(Core.bundle.get("settings.showSpawns"), true);
+				graphics.checkPref(Core.bundle.get("settings.possessionAllowed"), true);
+				graphics.checkPref(Core.bundle.get("settings.schematicsAllowed"), true);
+				graphics.checkPref(Core.bundle.get("settings.limitMapArea"), false);
 			}
 		);
 		var rules = Vars.state.rules;
 		Events.on(WorldLoadEvent.class, event -> worldFog = rules.fog);
 		Events.run(Trigger.update, () -> {
 				var settings = Core.settings;
-				rules.allowEditRules = settings.getBool("AllowEditRules");
-				rules.allowEditWorldProcessors = settings.getBool("AllowEditWorldProcessors");
-				if (Vars.player.unit() != null) Vars.player.unit().type.bounded = settings.getBool("Bounded");
+				rules.allowEditRules = settings.getBool(Core.bundle.get("settings.allowEditRules"));
+				rules.allowEditWorldProcessors = settings.getBool(Core.bundle.get("settings.allowEditWorldProcessors"));
+				if (Vars.player.unit() != null) Vars.player.unit().type.bounded = settings.getBool(Core.bundle.get("settings.bounded"));
 				if (worldFog) {
-					rules.fog = settings.getBool("Fog");
-					rules.staticFog = settings.getBool("StaticFog");
+					rules.fog = settings.getBool(Core.bundle.get("settings.fog"));
+					rules.staticFog = settings.getBool(Core.bundle.get("settings.staticFog"));
 				}
-				Vars.enableDarkness = settings.getBool("Darkness");
-				ControlPathfinder.showDebug = settings.getBool("ControlPathFinder");
-				rules.showSpawns = settings.getBool("ShowSpawns");
-				rules.possessionAllowed = settings.getBool("PossessionAllowed");
-				rules.schematicsAllowed = settings.getBool("SchematicsAllowed");
-				rules.limitMapArea = settings.getBool("LimitMapArea");
+				Vars.enableDarkness = settings.getBool(Core.bundle.get("settings.darkness"));
+				ControlPathfinder.showDebug = settings.getBool(Core.bundle.get("settings.controlPathFinder"));
+				rules.showSpawns = settings.getBool(Core.bundle.get("settings.showSpawns"));
+				rules.possessionAllowed = settings.getBool(Core.bundle.get("settings.possessionAllowed"));
+				rules.schematicsAllowed = settings.getBool(Core.bundle.get("settings.schematicsAllowed"));
+				rules.limitMapArea = settings.getBool(Core.bundle.get("settings.limitMapArea"));
 			}
 		);
 	}
