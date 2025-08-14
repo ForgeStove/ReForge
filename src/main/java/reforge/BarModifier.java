fb812598a840dbc05f3ef66449da117ea2932721
ForgeStove <ForgeStove@outlook.com>
2025-08-15 00:28:31 +0800

    优化 BarModifier 中的生命值和护甲条显示，更新版本号至 1.0.1

--- src/main/java/reforge/BarModifier.java
+++ src/main/java/reforge/BarModifier.java
@@ -8,13 +8,19 @@ import mindustry.ui.Bar;
 public class BarModifier {
 	public static void load() {
 		Events.on(
-			ContentInitEvent.class, event -> Vars.content.blocks().each(block -> block.addBar(
-				"health", entity -> new Bar(
-					() -> " " + Strings.autoFixed(entity.health(), 1) + " / " + Strings.autoFixed(entity.maxHealth(), 1),
-					() -> Pal.health,
-					entity::healthf
-				)
-			))
+			ContentInitEvent.class, event -> Vars.content.blocks().each(block -> {
+				block.addBar(
+					"health", building -> new Bar(
+						() -> "\uE813 " + Strings.autoFixed(building.health(), 1) + " / " + Strings.autoFixed(building.maxHealth(), 1),
+						() -> Pal.health,
+						building::healthf
+					)
+				);
+				if (block.armor != 0) block.addBar(
+					"armor",
+					building -> new Bar(() -> "\uE86B " + Strings.autoFixed(block.armor, 1), () -> Pal.ammo, () -> block.armor)
+				);
+			})
 		);
 	}
 }
