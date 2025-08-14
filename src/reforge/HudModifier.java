718caf6b7348404ca6983ea600f1984f38f3623f
ForgeStove <ForgeStove@outlook.com>
2025-08-14 12:26:15 +0800

    更新 build.gradle 版本至 1.0.1，调整本地化文件

--- src/reforge/HudModifier.java
+++ src/reforge/HudModifier.java
@@ -7,37 +7,37 @@ public class HudModifier {
 	public static boolean worldFog;
 	public static void load() {
 		Events.on(ClientLoadEvent.class, event -> {
-				var graphics = Vars.ui.settings.graphics;
-				graphics.checkPref(Core.bundle.get("settings.allowEditRules"), true);
-				graphics.checkPref(Core.bundle.get("settings.allowEditWorldProcessors"), true);
-				graphics.checkPref(Core.bundle.get("settings.bounded"), false);
-				graphics.checkPref(Core.bundle.get("settings.fog"), true);
-				graphics.checkPref(Core.bundle.get("settings.staticFog"), true);
-				graphics.checkPref(Core.bundle.get("settings.darkness"), true);
-				graphics.checkPref(Core.bundle.get("settings.controlPathFinder"), false);
-				graphics.checkPref(Core.bundle.get("settings.showSpawns"), true);
-				graphics.checkPref(Core.bundle.get("settings.possessionAllowed"), true);
-				graphics.checkPref(Core.bundle.get("settings.schematicsAllowed"), true);
-				graphics.checkPref(Core.bundle.get("settings.limitMapArea"), false);
+				var game = Vars.ui.settings.game;
+				game.checkPref(Core.bundle.get("allowEditRules"), true);
+				game.checkPref(Core.bundle.get("allowEditWorldProcessors"), true);
+				game.checkPref(Core.bundle.get("bounded"), false);
+				game.checkPref(Core.bundle.get("fog"), true);
+				game.checkPref(Core.bundle.get("staticFog"), true);
+				game.checkPref(Core.bundle.get("darkness"), true);
+				game.checkPref(Core.bundle.get("controlPathFinder"), false);
+				game.checkPref(Core.bundle.get("showSpawns"), true);
+				game.checkPref(Core.bundle.get("possessionAllowed"), true);
+				game.checkPref(Core.bundle.get("schematicsAllowed"), true);
+				game.checkPref(Core.bundle.get("limitMapArea"), false);
 			}
 		);
 		var rules = Vars.state.rules;
 		Events.on(WorldLoadEvent.class, event -> worldFog = rules.fog);
 		Events.run(Trigger.update, () -> {
 				var settings = Core.settings;
-				rules.allowEditRules = settings.getBool(Core.bundle.get("settings.allowEditRules"));
-				rules.allowEditWorldProcessors = settings.getBool(Core.bundle.get("settings.allowEditWorldProcessors"));
-				if (Vars.player.unit() != null) Vars.player.unit().type.bounded = settings.getBool(Core.bundle.get("settings.bounded"));
+				rules.allowEditRules = settings.getBool(Core.bundle.get("allowEditRules"));
+				rules.allowEditWorldProcessors = settings.getBool(Core.bundle.get("allowEditWorldProcessors"));
+				if (Vars.player.unit() != null) Vars.player.unit().type.bounded = settings.getBool(Core.bundle.get("bounded"));
 				if (worldFog) {
-					rules.fog = settings.getBool(Core.bundle.get("settings.fog"));
-					rules.staticFog = settings.getBool(Core.bundle.get("settings.staticFog"));
+					rules.fog = settings.getBool(Core.bundle.get("fog"));
+					rules.staticFog = settings.getBool(Core.bundle.get("staticFog"));
 				}
-				Vars.enableDarkness = settings.getBool(Core.bundle.get("settings.darkness"));
-				ControlPathfinder.showDebug = settings.getBool(Core.bundle.get("settings.controlPathFinder"));
-				rules.showSpawns = settings.getBool(Core.bundle.get("settings.showSpawns"));
-				rules.possessionAllowed = settings.getBool(Core.bundle.get("settings.possessionAllowed"));
-				rules.schematicsAllowed = settings.getBool(Core.bundle.get("settings.schematicsAllowed"));
-				rules.limitMapArea = settings.getBool(Core.bundle.get("settings.limitMapArea"));
+				Vars.enableDarkness = settings.getBool(Core.bundle.get("darkness"));
+				ControlPathfinder.showDebug = settings.getBool(Core.bundle.get("controlPathFinder"));
+				rules.showSpawns = settings.getBool(Core.bundle.get("showSpawns"));
+				rules.possessionAllowed = settings.getBool(Core.bundle.get("possessionAllowed"));
+				rules.schematicsAllowed = settings.getBool(Core.bundle.get("schematicsAllowed"));
+				rules.limitMapArea = settings.getBool(Core.bundle.get("limitMapArea"));
 			}
 		);
 	}
