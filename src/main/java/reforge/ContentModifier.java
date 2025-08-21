036debab1a563675db7dd971221748800f3c7b68
ForgeStove <ForgeStove@outlook.com>
2025-08-22 01:08:31 +0800

    重构内容加载逻辑，使用事件监听器处理内容初始化

--- src/main/java/reforge/ContentModifier.java
+++ src/main/java/reforge/ContentModifier.java
@@ -1,26 +1,32 @@
 package reforge;
+import arc.Events;
 import mindustry.Vars;
 import mindustry.content.UnitTypes;
+import mindustry.game.EventType.ContentInitEvent;
 import mindustry.world.blocks.distribution.Router;
 public class ContentModifier {
 	public static void load() {
 		Vars.maxSchematicSize = 1024;
-		var content = Vars.content;
-		content.blocks().each(block -> {
-			block.playerUnmineable = false;
-			block.replaceable = true;
-			block.inEditor = true;
-			if (block instanceof Router router) router.buildType = () -> router.new RouterBuild() {
-				public boolean canControl() {return true;}
-			};
-		});
-		content.items().each(item -> item.hidden = false);
-		content.liquids().each(liquid -> liquid.hidden = false);
-		content.units().each(unit -> {
-			if (unit == UnitTypes.block) return;
-			unit.hidden = false;
-			unit.rotateSpeed = 1024;
-			unit.weapons.each(weapon -> weapon.rotateSpeed = 1024);
-		});
+		Events.on(
+			ContentInitEvent.class, event -> {
+				var content = Vars.content;
+				content.blocks().each(block -> {
+					block.playerUnmineable = false;
+					block.replaceable = true;
+					block.inEditor = true;
+					if (block instanceof Router router) router.buildType = () -> router.new RouterBuild() {
+						public boolean canControl() {return true;}
+					};
+				});
+				content.items().each(item -> item.hidden = false);
+				content.liquids().each(liquid -> liquid.hidden = false);
+				content.units().each(unit -> {
+					if (unit == UnitTypes.block) return;
+					unit.hidden = false;
+					unit.rotateSpeed = 1024;
+					unit.weapons.each(weapon -> weapon.rotateSpeed = 1024);
+				});
+			}
+		);
 	}
 }
